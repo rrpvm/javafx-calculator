b20c5b11ad5b6bd44f169b60b391c125f4c411e2
rrpvm <funikovsasa@gmail.com>
2022-05-31 19:16:47 +0700

    prn

--- src/main/java/com/rrpvm/calculator/controller/MainController.java
+++ src/main/java/com/rrpvm/calculator/controller/MainController.java
@@ -1,18 +1,38 @@
 package com.rrpvm.calculator.controller;
-
-
+import com.rrpvm.calculator.service.CalculatorService;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-
+import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXML;
 public class MainController {
     @FXML
     private TextField expressionArea;
-    @FXML
-    public void onInput(ActionEvent event){
-        String txt = ((Button)event.getSource()).getText();
-        expressionArea.setText(expressionArea.getText() + txt);
 
+    @FXML
+    private void onInput(ActionEvent event) {
+        String txt = ((Button) event.getSource()).getText();
+        expressionArea.setText(expressionArea.getText() + txt);
+    }
+
+    @FXML
+    private void onClear(MouseEvent mouseEvent) {
+        expressionArea.clear();
+    }
+
+    @FXML
+    private void onErase(MouseEvent mouseEvent) {
+        if (expressionArea.getText().isEmpty()) return;
+        expressionArea.setText(expressionArea.getText(0, expressionArea.getText().length() - 1));
+    }
+    @FXML
+    private void onCalculateClicked(MouseEvent mouseEvent){
+        if(this.expressionArea.getText().isEmpty()){
+            //throw new exception
+            return;
+        }
+        CalculatorService calculatorService = CalculatorService.getInstance();
+        double result = calculatorService.calculate(this.expressionArea.getText());
+        this.expressionArea.setText(Double.toString(result));
     }
 }
---
+++ src/main/java/com/rrpvm/calculator/service/CalculatorService.java
@@ -0,0 +1,89 @@
+package com.rrpvm.calculator.service;
+
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CalculatorService {
+    private static CalculatorService instance;
+
+    private CalculatorService() {//only singleton
+
+    }
+
+    public static CalculatorService getInstance() {
+        if (instance == null) {
+            instance = new CalculatorService();
+        }
+        return instance;
+    }
+
+    private Stack<Character> parseOperations() {
+        return new Stack<Character>();
+    }
+
+    private int getCharacterPriority(char c) {
+        if (c == '+' || c == '-') return 2;
+        else if (c == '*' || c == '/') return 3;
+        return 0;
+    }
+
+    private String generateRNP(String expression) {
+        String rnp = "";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < expression.length(); ++i) {
+            int priority = getCharacterPriority(expression.charAt(i));
+            if (priority == 0) rnp += expression.charAt(i);
+            if (priority > 1) {
+                while (!stack.isEmpty()) {
+                    if (getCharacterPriority(stack.peek()) >= priority) rnp += stack.pop();
+                    else break;
+                }
+                stack.push(expression.charAt(i));
+                rnp += " ";
+            }
+        }
+        while (!stack.isEmpty()) rnp += stack.pop();
+        return rnp;
+    }
+
+    public double calculate(String expression) {
+        String rnpExpression = generateRNP(expression);
+        Stack<Character> operations = new Stack<>();
+        Stack<String> numbers = new Stack<>();
+        String str = "";
+        for (int i = 0; i < rnpExpression.length(); i++) {
+            char currentChar = rnpExpression.charAt(i);
+            int priority = getCharacterPriority(currentChar);
+            if (priority == 0) {
+                str += currentChar;
+            }
+            if (currentChar == ' ' || priority != 0) {
+                str = str.trim();
+                if (!str.isEmpty()) {
+                    numbers.push(str);
+                    str = "";
+                }
+            }
+            if (priority > 1) {
+                operations.push(currentChar);
+            }
+            if (!operations.isEmpty()) {
+                double right = Double.parseDouble(numbers.pop());
+                double left = Double.parseDouble(numbers.pop());
+                char operation = operations.pop();
+                if (operation == '+') {
+                    numbers.push(Double.toString(left + right));
+                } else if (operation == '-') {
+                    numbers.push(Double.toString(left - right));
+                } else if (operation == '/') {
+                    numbers.push(Double.toString(left / right));
+                } else if (operation == '*') {
+                    numbers.push(Double.toString(left * right));
+                }
+            }
+        }
+        return Double.parseDouble(numbers.peek());
+    }
+}
